99cbcfe6a9bcea7730ec34f447059010836aa8f2
benvhshed <47852378+benvstars@users.noreply.github.com>
2023-05-09 16:21:27 +0100

    add api endpoint for viewing all api endpoints as JSON

--- models/fetchCategories.model.js
+++ models/fetchCategories.model.js
@@ -1,6 +1,6 @@
 const db = require('../db')
 
-exports.fetchCatagories = () => {
+exports.fetchCategories = () => {
     return db.query('SELECT * FROM categories;').then((data) => {
         return data.rows;
     })
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getCatgories } = require('./controllers/getCatagories.controller');
+const { getCatgories } = require('./controllers/getCategories.controller');
+const { getEndpoints } = require('./controllers/getEndpoints.controller');
 const app = express();
 
 app.use(express.json());
 
-app.get('/api/catagories', getCatgories);
+app.get('/api', getEndpoints);
+app.get('/api/categories', getCatgories);
 
 app.use('/*', (req, res) => {
         res.status(404).send({message:'404 path not found'})
