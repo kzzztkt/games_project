8ccd43c68882e9b44ea4f16cd6f35858393ca70e
benvhshed <47852378+benvstars@users.noreply.github.com>
2023-05-12 15:48:45 +0100

    catch block

--- controllers/getUsers.controller.js
+++ controllers/getUsers.controller.js
@@ -1,9 +1,11 @@
 const { fetchUsers } = require("../models/fetchUsers.model")
 
-exports.getUsers =(req, res) => {
+exports.getUsers =(req, res, next) => {
 fetchUsers().then((result) => {
     res.status(200)
     .send({users:result});
+}).catcn((err) => {
+    next(err)
 })
   
 
